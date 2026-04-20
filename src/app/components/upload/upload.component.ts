e8abece3ffa955142cf76fa8203fd8ea66ceee20
Umair Safdar <thedevumair@gmail.com>
2026-04-20 21:49:28 +0500

    #3 upload service apu connection stable

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { UploadService } from '../../services/upload.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-upload',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss'
 })
 export class UploadComponent {
 
+  selectedFile!: File;
+  result: any;
+
+  constructor(private uploadService: UploadService) {}
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  upload() {
+    this.uploadService.uploadSRS(this.selectedFile).subscribe({
+      next: (res) => this.result = res,
+      error: (err) => console.error(err)
+    });
+  }
 }
